14140d31e5f3594359fe3c7f1f2918db95a5287c
oiranca <samuelromeroarbelo@gmail.com>
2024-10-26 19:44:33 +0100

    created add and delete box function

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,14 +1,26 @@
 import React from "react";
+import store from "../stores/MainStore";
+import getRandomColor from "../utils/getRandomColor";
+import uuid from "uuid/v4";
 
 function Toolbar() {
-  return (
-    <div className="toolbar">
-      <button>Add Box</button>
-      <button>Remove Box</button>
-      <input type="color" />
-      <span>No boxes selected</span>
-    </div>
-  );
+    const addNewBox = () => {
+        store.addBox({id: uuid(), color: getRandomColor(), left: 200, top: 50});
+    };
+
+    const deleteBox = () => {
+        store.deleteBox();
+
+    };
+
+    return (
+        <div className="toolbar">
+            <button onClick={addNewBox}>Add Box</button>
+            <button onClick={deleteBox}>Remove Box</button>
+            <input type="color"/>
+            <span>No boxes selected</span>
+        </div>
+    );
 }
 
 export default Toolbar;
--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -1,28 +1,31 @@
-import { types } from "mobx-state-tree";
+import {types} from "mobx-state-tree";
 import uuid from "uuid/v4";
 import BoxModel from "./models/Box";
 import getRandomColor from "../utils/getRandomColor";
 
 const MainStore = types
-  .model("MainStore", {
-    boxes: types.array(BoxModel)
-  })
-  .actions(self => {
-    return {
-      addBox(box) {
-        self.boxes.push(box);
-      }
-    };
-  })
-  .views(self => ({}));
+    .model("MainStore", {
+        boxes: types.array(BoxModel)
+    })
+    .actions(self => {
+        return {
+            addBox(box) {
+                self.boxes.push(box);
+            },
+            deleteBox() {
+                self.boxes.shift();
+            }
+        };
+    })
+    .views(self => ({}));
 
 const store = MainStore.create();
 
 const box1 = BoxModel.create({
-  id: uuid(),
-  color: getRandomColor(),
-  left: 0,
-  top: 0
+    id: uuid(),
+    color: getRandomColor(),
+    left: 0,
+    top: 0
 });
 
 store.addBox(box1);
